d60455e623981c1eeef62b384bd73a8f1c2d5578
Adam Biśta <410788@student.agh.edu.pl>
2024-05-09 19:00:15 +0200

    feat(startpage)[OK-39] Add responsiveness to sections

---
+++ frontend/src/components/startpage/JoinUs.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+const CallToAction: React.FC = () => {
+  const imageUrl = "https://picsum.photos/800/600"; // Using a random image from Lorem Picsum
+
+  return (
+    <section className="flex flex-col items-center justify-center min-h-[60vh] bg-palette1-darkest-blue-template1 text-palette1-creme-light relative lg:bottom-[100px]">
+      <h2 className="text-5xl font-bold mb-4 text-#292929" style={{letterSpacing:'1.8px'}}>Join Us!</h2>
+      <div className="relative w-64 h-64 mb-8 rounded-lg overflow-hidden border-2 border-palette1-creme-strong">
+        <Image
+          src={imageUrl}
+          alt="Join Us"
+          quality={75}
+          width={800}
+          height={600}
+        />
+      </div>
+
+      <Button variant="gold" className="text-2xl px-12" style={{padding: "1.06em 3.2em 0.9em",lineHeight: "20rem"}}>
+        Get Started!
+      </Button>
+    </section>
+  );
+};
+
+export default CallToAction;
---
+++ frontend/src/components/startpage/MissionStatement.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from './start.module.css'
+const MissionStatement: React.FC = () => {
+  return (
+    <section className="py-8 px-4 min-h-[90vh] relative  text-xl   bg-palette1-blue lg:clip-mission-statement flex flex-col justify-center lg:clip-mission-statement">
+      <div className="lg:w-7/12 md:w-55 relative z-10 flex flex-col gap-4 text-palette1-cream-template1">
+        <h2 className={`${styles['styled-h1']} text-5xl font-semibold`}>We Are The Future</h2>
+        <p className="mb-5 leading-9" style={{ letterSpacing:"1.2px"}}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commod. Vivamus tincidunt libero sodales nulla pretium
+          euismod. Fusce interdum commodo lacus eu facilisis. Curabitur quis
+          ultrices risus, eu porttitor diam. Etiam a elementum neque. Vivamus tincidunt libero sodales nulla pretium
+          euismod. Fusce interdum commodo lacus eu facilisis. 
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default MissionStatement;
