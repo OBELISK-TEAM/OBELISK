047b2ecc01704a34723fb4f30117ce69c2a5cf85
Pawel <pawel.konop@gmail.com>
2024-07-08 11:11:13 +0200

    feat(PR changes)[OK-21] Delete Member User Role

--- backend/src/enums/user.role.ts
+++ backend/src/enums/user.role.ts
@@ -2,9 +2,6 @@ export enum UserRole {
   // can change roles, delete users, etc.
   ADMIN = 100,
 
-  // can delete users, create/delete/modify boards, etc.
-  MEMBER = 10,
-
   // regular user can create/delete/modify/share boards, etc.
   USER = 1,
 }
