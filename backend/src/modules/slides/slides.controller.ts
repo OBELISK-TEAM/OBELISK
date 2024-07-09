9e15d92024b2240be06a9615486685ca0c8facca
Pawel <pawel.konop@gmail.com>
2024-07-09 19:39:30 +0200

    feat(slide CRUD)[OK-58] Add slide base endpoints

--- backend/src/modules/slides/slides.controller.ts
+++ backend/src/modules/slides/slides.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { SlidesService } from './slides.service';
 import { CreateSlideDto } from './slides.dto';
 
@@ -6,8 +15,28 @@ import { CreateSlideDto } from './slides.dto';
 export class SlidesController {
   constructor(private slidesService: SlidesService) {}
 
+  @Get()
+  async findAll(@Query('page') page: number) {
+    return this.slidesService.findAll(page);
+  }
+
   @Post()
   create(@Body() createSlideDto: CreateSlideDto) {
     return this.slidesService.create(createSlideDto);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') slideId: string) {
+    return this.slidesService.findOne(slideId);
+  }
+
+  @Put(':id')
+  Update(@Body() createSlideDto: CreateSlideDto, @Param('id') slideId: string) {
+    return this.slidesService.update(slideId, createSlideDto);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') slideId: string) {
+    return this.slidesService.delete(slideId);
   }
 }
--- backend/src/modules/slides/slides.service.ts
+++ backend/src/modules/slides/slides.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Slide } from '../../schemas/slide.schema';
@@ -6,10 +6,41 @@ import { CreateSlideDto } from './slides.dto';
 
 @Injectable()
 export class SlidesService {
+  private readonly limit = 5;
   constructor(@InjectModel(Slide.name) private slideModel: Model<Slide>) {}
+
+  async findAll(page: number = 1): Promise<Slide[]> {
+    const skip = (page - 1) * this.limit;
+    return this.slideModel.find().skip(skip).limit(this.limit).exec();
+  }
 
   async create(createSlideDto: CreateSlideDto): Promise<Slide> {
     const createdSlide = new this.slideModel(createSlideDto);
     return createdSlide.save();
+  }
+
+  async findOne(slideId: string): Promise<Slide> {
+    const existingSlide = await this.slideModel.findById(slideId).exec();
+    if (!existingSlide) throw new HttpException('Slide not found', 404);
+    return existingSlide;
+  }
+
+  async update(
+    slideId: string,
+    createSlideDto: CreateSlideDto,
+  ): Promise<Slide> {
+    const existingSlide = await this.slideModel
+      .findByIdAndUpdate(slideId, createSlideDto, { new: true })
+      .exec();
+    if (!existingSlide) throw new HttpException('Slide not found', 404);
+    return existingSlide;
+  }
+
+  async delete(slideId: string): Promise<Slide> {
+    const existingSlide = await this.slideModel
+      .findByIdAndDelete(slideId)
+      .exec();
+    if (!existingSlide) throw new HttpException('Slide not found', 404);
+    return existingSlide;
   }
 }
