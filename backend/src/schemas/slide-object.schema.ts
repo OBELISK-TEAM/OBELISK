f01a78f7b441a2fc44d133cc240c2f41f8503765
Wiktor Wilkusz <wiktorwilkusz13@gmail.com>
2024-07-31 22:08:14 +0200

    feat(schemas)[OK-63] Add SlideObject schema (#25)

---
+++ backend/src/schemas/slide-object.schema.ts
@@ -0,0 +1,359 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { User } from './user.schema';
+import { Slide } from './slide.schema';
+import { SlideObjectShadow } from 'src/shared/interfaces/SlideObjectShadow';
+import {
+  Schema as MongooseSchema,
+  Document as MongooseDocument,
+} from 'mongoose';
+
+export type SlideObjectDocument = SlideObject & MongooseDocument;
+
+@Schema({ timestamps: true })
+export class SlideObject {
+
+  // Fabric.js-specific properties
+
+  @Prop({
+    required: true,
+  })
+  type: string;
+
+  @Prop({
+    required: true,
+  })
+  version: string;
+
+  @Prop({
+    required: true,
+  })
+  originX: string;
+
+  @Prop({
+    required: true,
+  })
+  originY: string;
+
+  @Prop({
+    required: true,
+  })
+  left: number;
+
+  @Prop({
+    required: true,
+  })
+  top: number;
+
+  @Prop({
+    required: true,
+  })
+  width: number;
+
+  @Prop({
+    required: true,
+  })
+  height: number;
+
+  @Prop({
+    type: String,
+    default: null,
+  })
+  fill: string | null;
+
+  @Prop({
+    type: String,
+    default: null,
+  })
+  stroke: string | null;
+
+  @Prop({
+    required: true,
+  })
+  strokeWidth: number;
+
+  @Prop({
+    type: [Number],
+    default: null,
+  })
+  strokeDashArray: number[] | null;
+
+  @Prop({
+    required: true,
+    enum: ['butt', 'round', 'square'],
+  })
+  strokeLineCap: 'butt' | 'round' | 'square';
+
+  @Prop({
+    required: true,
+  })
+  strokeDashOffset: number;
+
+  @Prop({
+    required: true,
+    enum: ['miter', 'round', 'bevel'],
+  })
+  strokeLineJoin: 'miter' | 'round' | 'bevel';
+
+  @Prop({
+    required: true,
+  })
+  strokeUniform: boolean;
+
+  @Prop({
+    required: true,
+  })
+  strokeMiterLimit: number;
+
+  @Prop({
+    required: true,
+  })
+  scaleX: number;
+
+  @Prop({
+    required: true,
+  })
+  scaleY: number;
+
+  @Prop({
+    required: true,
+  })
+  angle: number;
+
+  @Prop({
+    required: true,
+  })
+  flipX: boolean;
+
+  @Prop({
+    required: true,
+  })
+  flipY: boolean;
+
+  @Prop({
+    required: true,
+  })
+  opacity: number;
+
+  @Prop({
+    type: SlideObjectShadow,
+    default: null,
+  })
+  shadow: SlideObjectShadow | null;
+
+  @Prop({
+    required: true,
+  })
+  visible: boolean;
+
+  @Prop({
+    required: true,
+  })
+  backgroundColor: string;
+
+  @Prop({
+    required: true,
+  })
+  fillRule: string;
+
+  @Prop({
+    required: true,
+    enum: ['fill', 'stroke'],
+  })
+  paintFirst: 'fill' | 'stroke';
+
+  @Prop({
+    required: true,
+  })
+  globalCompositeOperation: string;
+
+  @Prop({
+    required: true,
+  })
+  skewX: number;
+
+  @Prop({
+    required: true,
+  })
+  skewY: number;
+
+  @Prop({
+    type: [[String, Number, Number]],
+    required: true,
+  })
+  path: Array<[string, number, number]>;
+
+  @Prop({
+    type: Number,
+  })
+  x1?: number;
+
+  @Prop({
+    type: Number,
+  })
+  x2?: number;
+
+  @Prop({
+    type: Number,
+  })
+  y1?: number;
+
+  @Prop({
+    type: Number,
+  })
+  y2?: number;
+
+  @Prop({
+    type: Number,
+  })
+  rx?: number;
+
+  @Prop({
+    type: Number,
+  })
+  ry?: number;
+
+  @Prop({
+    type: Number,
+  })
+  radius?: number;
+
+  @Prop({
+    type: Number,
+  })
+  startAngle?: number;
+
+  @Prop({
+    type: Number,
+  })
+  endAngle?: number;
+
+  @Prop({
+    type: String,
+  })
+  fontFamily?: string;
+
+  @Prop({
+    type: String,
+  })
+  fontWeight?: string;
+
+  @Prop({
+    type: Number,
+  })
+  fontSize?: number;
+
+  @Prop({
+    type: String,
+  })
+  text?: string;
+
+  @Prop({
+    type: Boolean,
+  })
+  underline?: boolean;
+
+  @Prop({
+    type: Boolean,
+  })
+  overline?: boolean;
+
+  @Prop({
+    type: Boolean,
+  })
+  linethrough?: boolean;
+
+  @Prop({
+    enum: ['left', 'center', 'right'],
+  })
+  textAlign?: 'left' | 'center' | 'right';
+
+  @Prop({
+    enum: ['normal', 'italic'],
+  })
+  fontStyle?: 'normal' | 'italic';
+
+  @Prop({
+    type: Number,
+  })
+  lineHeight?: number;
+
+  @Prop({
+    type: String,
+  })
+  textBackgroundColor?: string;
+
+  @Prop({
+    type: Number,
+  })
+  charSpacing?: number;
+
+  @Prop({
+    type: MongooseSchema.Types.Mixed,
+  })
+  styles?: {
+    [key: string]: {
+      textDecoration: string;
+    };
+  }[];
+
+  @Prop({
+    enum: ['ltr', 'rtl'],
+  })
+  direction?: 'ltr' | 'rtl';
+
+  @Prop({
+    type: Number,
+  })
+  pathStartOffset?: number;
+
+  @Prop({
+    enum: ['left', 'right', 'center'],
+  })
+  pathSide?: 'left' | 'right' | 'center';
+
+  @Prop({
+    enum: ['baseline', 'middle', 'top', 'bottom'],
+  })
+  pathAlign?: 'baseline' | 'middle' | 'top' | 'bottom';
+
+  @Prop({
+    type: String,
+  })
+  src?: string;
+
+  @Prop({
+    type: Number,
+  })
+  cropX?: number;
+
+  @Prop({
+    type: Number,
+  })
+  cropY?: number;
+
+  @Prop({
+    type: [{ type: MongooseSchema.Types.Mixed }],
+  })
+  filters?: {
+    type: string;
+    value: number;
+  }[];
+
+  // relations
+
+  @Prop({
+    required: true,
+    type: MongooseSchema.Types.ObjectId,
+    ref: 'User',
+  })
+  createdBy: User;
+
+  @Prop({
+    required: true,
+    type: MongooseSchema.Types.ObjectId,
+    ref: 'Slide',
+  })
+  slide: Slide;
+}
+
+export const SlideObjectSchema = SchemaFactory.createForClass(SlideObject);
--- backend/src/schemas/slide.schema.ts
+++ backend/src/schemas/slide.schema.ts
@@ -1,82 +1,12 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Board } from './board.schema';
+import { SlideObject } from './slide-object.schema';
+import {
+  Schema as MongooseSchema,
+  Document as MongooseDocument,
+} from 'mongoose';
 
-export class SlideObject {
-  type: string;
-  version: string;
-  originX: string;
-  originY: string;
-  left: number;
-  top: number;
-  width: number;
-  height: number;
-  fill: string | null;
-  stroke: string | null;
-  strokeWidth: number;
-  strokeDashArray: number[] | null;
-  strokeLineCap: 'butt' | 'round' | 'square';
-  strokeDashOffset: number;
-  strokeLineJoin: 'miter' | 'round' | 'bevel';
-  strokeUniform: boolean;
-  strokeMiterLimit: number;
-  scaleX: number;
-  scaleY: number;
-  angle: number;
-  flipX: boolean;
-  flipY: boolean;
-  opacity: number;
-  shadow: {
-    color: string;
-    blur: number;
-    offsetX: number;
-    offsetY: number;
-  } | null;
-  visible: boolean;
-  backgroundColor: string;
-  fillRule: string;
-  paintFirst: 'fill' | 'stroke';
-  globalCompositeOperation: string;
-  skewX: number;
-  skewY: number;
-  path: Array<[string, number, number]>;
-  x1?: number;
-  x2?: number;
-  y1?: number;
-  y2?: number;
-  rx?: number;
-  ry?: number;
-  radius?: number;
-  startAngle?: number;
-  endAngle?: number;
-  fontFamily?: string;
-  fontWeight?: string;
-  fontSize?: number;
-  text?: string;
-  underline?: boolean;
-  overline?: boolean;
-  linethrough?: boolean;
-  textAlign?: 'left' | 'center' | 'right';
-  fontStyle?: 'normal' | 'italic';
-  lineHeight?: number;
-  textBackgroundColor?: string;
-  charSpacing?: number;
-  styles?: {
-    [key: string]: {
-      textDecoration: string;
-    };
-  }[];
-  direction?: 'ltr' | 'rtl';
-  pathStartOffset?: number;
-  pathSide?: 'left' | 'right' | 'center';
-  pathAlign?: 'baseline' | 'middle' | 'top' | 'bottom';
-  src?: string;
-  cropX?: number;
-  cropY?: number;
-  filters?: {
-    type: string;
-    value: number;
-  }[];
-}
+export type SlideDocument = Slide & MongooseDocument
 
 @Schema({ timestamps: true })
 export class Slide {
@@ -90,8 +20,9 @@ export class Slide {
   @Prop({
     type: [
       {
-        type: SlideObject,
+        type: MongooseSchema.Types.ObjectId,
         required: true,
+        ref: 'SlideObject'
       },
     ],
   })
