92aaa188a03c06d691027a90ba12066f9dbfe893
Adam Biśta <410788@student.agh.edu.pl>
2024-08-03 18:33:30 +0200

    fix(GoogleLogin)[OK-57] Fix logic for login via google

--- frontend/src/components/GoogleLogin/GoogleLogin.tsx
+++ frontend/src/components/GoogleLogin/GoogleLogin.tsx
@@ -1,38 +1,50 @@
-'use client';
-import { useCallback, useState,useEffect } from 'react';
-import { redirect } from 'next/navigation'
-import axios from 'axios';
-import GoogleIcon from "@/components/non-lucid-icons/GoogleIcon";
+"use client";
+import { redirect } from "next/navigation";
+import axios from "axios";
+import React from "react";
 import {Button} from "@/components/ui/button";
 
 const GoogleLogin = () => {
-  const [tempUserId, setTempUserId] = useState<string>('');
-
-
-  const loginGoogleUser = useCallback(async (userTempId: string) => {
+  const loginGoogleUser = async (userTempId: string) => {
     try {
-      const response = await axios.post('http://localhost:4000/auth/google/login', {}, {
-        headers: {
-          Authorization: `Bearer ${userTempId}`
-        }
-      });
+      console.log("123123123");
+      const response = await axios.post(
+          "http://localhost:4000/auth/google/login",
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${userTempId}`,
+            },
+          },
+      );
       alert("You are logged in.");
-      localStorage.setItem('token', response.data.token);
-      redirect('/user-boards');
+      localStorage.setItem("token", response.data.token);
+      redirect("/user-boards");
     } catch (error: any) {
-      alert(error.response?.data?.message || 'Login failed');
+      alert(error.response?.data?.message || "Login failed");
     }
-  },[]);
+  };
 
-  const googleAuth = async () => {
-    const url = getGoogleLoginUrl();
-    const authWindow = window.open(url, '', 'popup=true');
-    if (authWindow) {
-      const checkPopupWindow = setInterval(() => {
-        if (authWindow.closed) {
-          clearInterval(checkPopupWindow);
-          if (tempUserId) loginGoogleUser(tempUserId);
-          setTempUserId('');
+  const googleAuth = () => {
+    console.log("23123123");
+    const { url, state } = getGoogleLoginUrl();
+    console.log(url);
+
+    const newAuthWindow = window.open(url, "", "popup=true");
+
+    if (newAuthWindow) {
+      const checkPopupWindow = setInterval(async () => {
+        try {
+          if (newAuthWindow.closed) {
+            clearInterval(checkPopupWindow);
+            if (state) {
+              await loginGoogleUser(state);
+            }
+          } else {
+            newAuthWindow.postMessage("ping", "*");
+          }
+        } catch (e) {
+          console.log(e);
         }
       }, 100);
     }
@@ -40,32 +52,39 @@ const GoogleLogin = () => {
 
   const getGoogleLoginUrl = () => {
     const state = crypto.randomUUID();
-    setTempUserId(state);
-    const googleAuthEndpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+    const googleAuthEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+
+    const scope = [
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile",
+    ].join(" ");
+
     const loginRequestParameters: { [key: string]: string } = {
-      response_type: 'code',
-      redirect_uri: process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI!,
-      scope: process.env.NEXT_PUBLIC_GOOGLE_SCOPE!,
-      client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
-      state
+      response_type: "code",
+      redirect_uri: "http://localhost:4000/auth/google/callback",
+      scope,
+      client_id:
+          "1023376529403-8vkrercdkg68t8v8ld9kqrtvt7emo24h.apps.googleusercontent.com",
+      state,
     };
+
     const paramsString = Object.keys(loginRequestParameters)
-      .map((key) => `${key}=${encodeURIComponent(loginRequestParameters[key])}`)
-      .join('&');
-    return `${googleAuthEndpoint}?${paramsString}`;
+        .map((key) => `${key}=${encodeURIComponent(loginRequestParameters[key])}`)
+        .join("&");
+    return { url: `${googleAuthEndpoint}?${paramsString}`, state };
   };
 
-  useEffect(() => {
-    window.addEventListener('message', (event) => {
-      if (event.origin !== window.location.origin) return;
-      if (event.data && event.data.tempUserId) {
-        loginGoogleUser(event.data.tempUserId);
-      }
-    });
-  }, [loginGoogleUser]);
-
   return (
-      <Button variant="outline" onClick={googleAuth} ><GoogleIcon width={16} height={16} /> &ensp;Google</Button>
+        <Button variant="outline"
+            onClick={googleAuth}
+        >
+          <img
+              src="https://developers.google.com/identity/images/g-logo.png"
+              alt="Google logo"
+              className="mr-3 w-5 h-5"
+          />
+          Google
+        </Button>
   );
 };
 
