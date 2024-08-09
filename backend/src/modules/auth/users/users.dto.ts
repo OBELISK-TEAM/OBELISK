99aa63b2e045521f6deb5de8e08ddbfb3f3bb0c5
Pawel <pawel.konop@gmail.com>
2024-08-09 17:21:49 +0200

    style(dto)[OK-90] Add custom messages

--- backend/src/modules/auth/users/users.dto.ts
+++ backend/src/modules/auth/users/users.dto.ts
@@ -7,19 +7,35 @@ import {
 } from 'class-validator';
 
 export class CreateUserDto {
-  @IsEmail()
-  @IsNotEmpty()
-  @Length(6, 30)
+  @IsEmail(
+    {},
+    {
+      message: 'The email is invalid',
+    },
+  )
+  @IsNotEmpty({
+    message: 'The email is required',
+  })
+  @Length(6, 30, {
+    message: 'The email must be at least 6 but not longer than 30 characters',
+  })
   email: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @Length(8, 30)
+  @IsString({
+    message: 'The password must be a string',
+  })
+  @IsNotEmpty({
+    message: 'The password is required',
+  })
+  @Length(8, 30, {
+    message:
+      'The password must be at least 8 but not longer than 30 characters',
+  })
   @Matches(
     /^(?=(?:.*[A-Z]){2})(?=(?:.*[a-z]){2})(?=(?:.*\d){2})(?=(?:.*\W){2}).*$/,
     {
       message:
-        'the password must contain at least 2 special characters, 2 uppercase letters and 2 digits',
+        'The password must contain at least 2 special characters, 2 uppercase letters and 2 digits',
     },
   )
   password: string;
--- backend/src/modules/slides/slides.dto.ts
+++ backend/src/modules/slides/slides.dto.ts
@@ -2,24 +2,40 @@ import { IsNotEmpty, IsString } from 'class-validator';
 import { SlideObject } from '../../schemas/slide-object.schema';
 
 export class CreateSlideDto {
-  @IsString()
-  @IsNotEmpty()
+  @IsString({
+    message: 'The version must be a string',
+  })
+  @IsNotEmpty({
+    message: 'The version is required',
+  })
   version: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({
+    message: 'The boardId must be a string',
+  })
+  @IsNotEmpty({
+    message: 'The boardId is required',
+  })
   boardId: string;
 
   objects: SlideObject[];
 }
 
 export class UpdateSlideDto {
-  @IsString()
-  @IsNotEmpty()
+  @IsString({
+    message: 'The version must be a string',
+  })
+  @IsNotEmpty({
+    message: 'The version is required',
+  })
   version: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({
+    message: 'The boardId must be a string',
+  })
+  @IsNotEmpty({
+    message: 'The boardId is required',
+  })
   boardId: string;
 
   objects: SlideObject[];
