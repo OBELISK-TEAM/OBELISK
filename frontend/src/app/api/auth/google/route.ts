4f8f51e8c180ac4d658346cc6d0d75293ad9f14b
Adam Biśta <410788@student.agh.edu.pl>
2024-08-09 23:03:45 +0200

    fix(ApiRoute)[OK-57] Wrap logic with try catch block

--- frontend/src/app/api/auth/google/route.ts
+++ frontend/src/app/api/auth/google/route.ts
@@ -2,34 +2,46 @@ import { NextRequest, NextResponse } from "next/server";
 import { setTokenCookie } from "@/lib/authApi";
 
 export async function POST(req: NextRequest) {
-  const { state } = await req.json();
-  // console.log(
-  //   "endpoint",
-  //   `http://${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}/auth/google/login`,
-  // );
-  // console.log("method", "POST");
-  // console.log("headers", {
-  //   "Content-Type": "application/json",
-  //   Authorization: `Bearer ${state}`,
-  // });
+  try {
+    const { state } = await req.json();
 
-  const response = await fetch(
-    `http://${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}/auth/google/login`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${state}`,
+    console.log(
+      "endpoint",
+      `http://${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}/auth/google/login`,
+    );
+    // console.log("method", "POST");
+    // console.log("headers", {
+    //   "Content-Type": "application/json",
+    //   Authorization: `Bearer ${state}`,
+    // });
+
+    const response = await fetch(
+      `http://${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}/auth/google/login`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${state}`,
+        },
       },
-    },
-  );
+    );
 
-  if (response.ok) {
-    const { accessToken } = await response.json();
-    setTokenCookie(accessToken);
-    return NextResponse.json({ message: "Login successful" });
-  } else {
-    const error = await response.json();
-    return NextResponse.json(error, { status: response.status });
+    if (response.ok) {
+      const { accessToken } = await response.json();
+      setTokenCookie(accessToken);
+      return NextResponse.json({ message: "Login successful" });
+    } else {
+      console.error("Promise resolved but HTTP status failed");
+      const error = await response.json();
+      return NextResponse.json(error, {
+        status: response.status,
+      } as ResponseInit);
+    }
+  } catch (error) {
+    console.error("Promise rejected", error);
+    return NextResponse.json(
+      { message: "An unexpected error occurred" },
+      { status: 500 },
+    );
   }
 }
