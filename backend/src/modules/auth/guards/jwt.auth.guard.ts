6e643126d777abef19067b3a6da52540073c397a
Pawel <pawel.konop@gmail.com>
2024-08-15 19:19:11 +0200

    ci[OK-101] Add types to pass CI

--- backend/src/modules/auth/guards/jwt.auth.guard.ts
+++ backend/src/modules/auth/guards/jwt.auth.guard.ts
@@ -4,6 +4,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { UserRole } from '../../../enums/user.role';
 import { SafeUserDoc } from '../../../shared/interfaces/SafeUserDoc';
 import { ROLE_KEY } from '../decorators/roles.decorator';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
@@ -11,30 +12,33 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     super();
   }
 
-  async canActivate(context: ExecutionContext): Promise<any> {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    // parent canActivate method
     const canActivate = await super.canActivate(context);
 
     if (!canActivate) return false;
 
+    // check if controller or handler has @Role() decorator
+    // (custom canActivate method)
     const requiredRole = this.reflector.get<UserRole>(
       ROLE_KEY,
       context.getHandler(),
     );
 
-    // if no role is required, then the user is authorized (means no @Role() decorator)
+    // if no role is required, then the user is authorized
+    // - means no @Role() decorator
     if (!requiredRole) return true;
 
-    const request = context.switchToHttp().getRequest();
-    const user: SafeUserDoc = request.user;
+    const request = context.switchToHttp().getRequest<Request>();
+    const user = request.user as SafeUserDoc;
 
     // can switch to greater than or equal to allow for multiple roles
     // if USER is 1, MEMBER is 10, and ADMIN is 100, then
     // if the required role is MEMBER, and the user is ADMIN, it will pass
     // other idea is to provide array of roles, and check if the user has any of the roles
-    // let me know if you want me to implement that instead
-    if (user.userRole !== requiredRole) {
+    if (user.userRole !== requiredRole)
       throw new HttpException('Forbidden resource', 403);
-    }
+
     return true;
   }
 }
