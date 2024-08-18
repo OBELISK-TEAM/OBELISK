febfe8c788eec872870b9c266f80635e9781865f
Adam Biśta <410788@student.agh.edu.pl>
2024-08-18 23:30:21 +0200

    refactor(drawingMode)[OK-89] Refactor function

--- frontend/src/utils/drawingMode.ts
+++ frontend/src/utils/drawingMode.ts
@@ -1,11 +1,11 @@
 import { CanvasMode } from "@/enums/CanvasMode";
 
-export const isActiveItem = (itemName: string, activeItem: string | null, canvasMode: CanvasMode): boolean => {
-  return (
-    itemName === activeItem ||
-    (itemName === CanvasMode.Selection && canvasMode === CanvasMode.Selection) ||
-    (itemName === CanvasMode.SimpleDrawing && canvasMode === CanvasMode.SimpleDrawing) ||
-    (itemName === CanvasMode.Eraser && canvasMode === CanvasMode.Eraser)
-  );
+export const isActiveItem = (
+  itemName: string,
+  activeItem: string | null,
+  canvasMode: CanvasMode
+): boolean => {
+  return itemName === activeItem || itemName === canvasMode.toString();
 };
+
 
