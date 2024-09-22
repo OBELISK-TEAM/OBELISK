d41892275013c60ba51c84267c2508ba13a4cb08
Adam Biśta <410788@student.agh.edu.pl>
2024-09-22 20:42:15 +0200

    fix(Frontend)[OK-94] Resolve threads

--- client/src/components/ui/pagination.tsx
+++ client/src/components/ui/pagination.tsx
@@ -28,7 +28,6 @@ PaginationItem.displayName = "PaginationItem";
 
 type PaginationLinkProps = {
   isActive?: boolean;
-  text?: string;
 } & Pick<ButtonProps, "size"> &
   React.ComponentProps<"a">;
 
@@ -56,9 +55,9 @@ const PaginationPrevious = ({ className, ...props }: React.ComponentProps<typeof
 );
 PaginationPrevious.displayName = "PaginationPrevious";
 
-const PaginationNext = ({ className, text, ...props }: React.ComponentProps<typeof PaginationLink>) => (
+const PaginationNext = ({ className, ...props }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink aria-label="Go to next page" size="default" className={cn("gap-1 pr-2.5", className)} {...props}>
-    <span>{text ? `${text}` : "Next"}</span>
+    <span>Next</span>
     <ChevronRight className="h-4 w-4" />
   </PaginationLink>
 );
