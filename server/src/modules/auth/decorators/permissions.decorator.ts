329fd38145c2c083b9dce0bb69821ddfe93812bb
Pawel <pawel.konop@gmail.com>
2024-09-25 23:50:33 +0200

    feat(ws)[OK-165] Add guard for board permission

---
+++ server/src/modules/auth/decorators/permissions.decorator.ts
@@ -0,0 +1,7 @@
+import { BoardPermission } from '../../../enums/board.permission';
+import { SetMetadata } from '@nestjs/common';
+
+export const MINIMUM_BOARD_PERMISSION_KEY = 'minimumBoardPermission';
+
+export const MinimumBoardPermission = (permission: BoardPermission) =>
+  SetMetadata(MINIMUM_BOARD_PERMISSION_KEY, permission);
---
+++ server/src/modules/auth/guards/board.permission.guard.ts
@@ -0,0 +1,48 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { WsException } from '@nestjs/websockets';
+import { BoardPermission } from '../../../enums/board.permission';
+import { GwSocketWithTarget } from '../../../shared/interfaces/auth/GwSocket';
+import { MINIMUM_BOARD_PERMISSION_KEY } from '../decorators/permissions.decorator';
+
+@Injectable()
+export class BoardPermissionGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const client: GwSocketWithTarget = context.switchToWs().getClient();
+    const user = client.data.user;
+
+    if (!user || !user.availableBoards || !user.targetBoard) {
+      throw new WsException('Try to reconnect');
+    }
+
+    const minimumPermission = this.getMinimumPermission(context);
+    if (minimumPermission === undefined) return true;
+    return this.validatePermission(client, minimumPermission);
+  }
+
+  private getMinimumPermission(
+    context: ExecutionContext,
+  ): BoardPermission | undefined {
+    return this.reflector.get<BoardPermission>(
+      MINIMUM_BOARD_PERMISSION_KEY,
+      context.getHandler(),
+    );
+  }
+
+  private validatePermission(
+    client: GwSocketWithTarget,
+    requiredPermission: BoardPermission,
+  ): boolean {
+    const userBoardPermission = client.data.user.targetBoard.permission;
+    if (userBoardPermission < requiredPermission) {
+      client.emit('error', {
+        message: `You need at least ${BoardPermission[requiredPermission]} permission to perform this action`,
+      });
+      client.disconnect(true);
+      return false;
+    }
+    return true;
+  }
+}
