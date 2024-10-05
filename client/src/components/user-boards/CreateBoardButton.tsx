144f3138d40df538b886fe42525defb5cc92b4e0
Adam Biśta <410788@student.agh.edu.pl>
2024-10-05 21:17:04 +0200

    fix(Buttons)[OK-159] Remove unused buttons

---
+++ client/src/components/user-boards/CreateBoardButton.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
+import { CreateBoardDialog } from "@/components/user-boards/CreateBoardDialog";
+import { useCreateBoard } from "@/contexts/CreateBoardContext";
+
+export const CreateBoardButton = () => {
+  const { isLoading, createNewBoard } = useCreateBoard();
+
+  return (
+    <CreateBoardDialog action={createNewBoard}>
+      <Button disabled={isLoading}>
+        <PlusIcon className="mr-2 h-5 w-5" />
+        {isLoading ? "Creating..." : "Create new board"}
+      </Button>
+    </CreateBoardDialog>
+  );
+};
+
+export default CreateBoardButton;
