b0dd080a63e31b2bec4c41494ad3b8e087bf311f
Adam Biśta <410788@student.agh.edu.pl>
2024-10-06 12:29:15 +0200

    feat(Sidebar)[OK-187] Implement skeleton of sidebar

--- client/src/app/user-boards/[boardId]/layout.tsx
+++ client/src/app/user-boards/[boardId]/layout.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { BoardDetailsSidebar } from "@/components/board-details/BoardDetailsSidebar";
 interface LayoutProps {
   children: React.ReactNode;
 }
 const BoardLayout = ({ children }: LayoutProps) => {
   return (
     <div className="flex flex-1">
+      <BoardDetailsSidebar />
       <main className="flex-1 overflow-auto p-6">{children}</main>
     </div>
   );
