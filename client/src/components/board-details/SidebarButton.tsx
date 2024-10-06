f354f655f8b3d3c6cc6ee80d220fdc1155ddd3e0
Adam Biśta <410788@student.agh.edu.pl>
2024-10-06 15:18:11 +0200

    fix(BoardSidebar)[OK-188] Move isActive to higher component in component tree

--- client/src/components/board-details/SidebarButton.tsx
+++ client/src/components/board-details/SidebarButton.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { ReactNode } from "react";
 import { cn } from "@/lib/utils";
@@ -9,19 +8,12 @@ import { Button } from "@/components/ui/button";
 interface SidebarButtonProps {
   href: string;
   children: ReactNode;
+  isActive: boolean;
 }
 
-export const SidebarButton = ({ href, children }: SidebarButtonProps) => {
-  const pathname = usePathname();
-  const isActive = () => {
-    const normalizedPath = pathname.endsWith("/") ? pathname.slice(0, -1) : pathname;
-    const normalizedHref = href.endsWith("/") ? href.slice(0, -1) : href;
-
-    return normalizedPath === normalizedHref;
-  };
-
+export const SidebarButton = ({ href, children, isActive }: SidebarButtonProps) => {
   return (
-    <Button asChild variant="ghost" className={cn("p-2", isActive() ? "bg-muted text-foreground" : "text-foreground")}>
+    <Button asChild variant="ghost" className={cn("p-2", isActive ? "bg-muted text-foreground" : "text-foreground")}>
       <Link href={href}>
         <div className="flex w-full items-center">{children}</div>
       </Link>
