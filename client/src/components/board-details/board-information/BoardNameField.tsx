82ab838d7198baeccf0db34601e186de914d64d3
Adam Biśta <410788@student.agh.edu.pl>
2024-10-07 23:33:01 +0200

    refactor(BoardDetails)[OK-209] Add reusable components

--- client/src/components/board-details/board-information/BoardNameField.tsx
+++ client/src/components/board-details/board-information/BoardNameField.tsx
@@ -1,76 +1,87 @@
-import React, { KeyboardEvent } from "react";
+"use client";
+import React, { KeyboardEvent, useEffect } from "react";
 import { Square, Pencil, Save, XCircle } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import BoardInfoItem from "./BoardInfoItem";
+import BoardInfoInputItem from "@/components/board-details/board-information/BoardInfoInputItem";
+import { useBoardName } from "@/hooks/board-details/useBoardName";
+import { BoardResponse } from "@/interfaces/responses/user-boards/board-response";
 
 interface BoardNameFieldProps {
-  name: string;
-  isEditing: boolean;
-  updating: boolean;
-  setName: (name: string) => void;
-  handleKeyDown: (e: KeyboardEvent<HTMLInputElement>) => void;
-  handleEditClick: () => void;
-  handleConfirm: () => void;
-  handleCancel: () => void;
+  board: BoardResponse | undefined;
+  id: string;
+  mutate: () => void;
 }
 
-const BoardNameField: React.FC<BoardNameFieldProps> = ({
-  name,
-  isEditing,
-  updating,
-  setName,
-  handleKeyDown,
-  handleEditClick,
-  handleConfirm,
-  handleCancel,
-}) => (
-  <BoardInfoItem
-    icon={<Square className="mr-1 h-4 w-4" />}
-    label="Board name"
-    value={name}
-    isEditing={isEditing}
-    onKeyDown={handleKeyDown}
-    inputProps={{
-      id: "board-name",
-      onChange: (e) => setName(e.target.value),
-      placeholder: "Board Name",
-    }}
-    actions={
-      isEditing ? (
-        <div className="ml-2 flex items-center gap-2">
-          <Button
-            aria-label="Confirm name"
-            onClick={handleConfirm}
-            className={updating ? "cursor-not-allowed opacity-50" : ""}
-            disabled={updating}
-          >
-            <div className="flex items-center gap-2">
-              <Save size={12} />
-              <span>Save</span>
-            </div>
-          </Button>
-          <Button
-            aria-label="Cancel edit"
-            variant="secondary"
-            onClick={handleCancel}
-            className={updating ? "cursor-not-allowed opacity-50" : ""}
-          >
-            <div className="flex items-center gap-2">
-              <XCircle size={12} />
-              <span>Cancel</span>
-            </div>
-          </Button>
-        </div>
-      ) : (
-        <Button variant="secondary" aria-label="Edit name" onClick={handleEditClick} className="ml-2">
-          <div className="flex items-center gap-2">
-            <Pencil size={12} />
-            <span>Edit</span>
-          </div>
-        </Button>
-      )
+const BoardNameField: React.FC<BoardNameFieldProps> = ({ board, id, mutate }) => {
+  const { name, isEditing, updating, setName, handleEditClick, handleCancel, handleConfirm } = useBoardName(
+    board,
+    mutate
+  );
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleConfirm();
+    } else if (e.key === "Escape") {
+      handleCancel();
     }
-  />
-);
+  };
+
+  useEffect(() => {
+    if (board) {
+      setName(board.name);
+    }
+  }, [board, setName]);
+
+  return (
+    <BoardInfoInputItem
+      icon={<Square className="mr-1 h-4 w-4" />}
+      label="Board name"
+      value={name}
+      isEditing={isEditing}
+      onKeyDown={handleKeyDown}
+      id={id}
+      inputProps={{
+        id: "board-name",
+        placeholder: "Board Name",
+        onChange: (e) => setName(e.target.value),
+      }}
+      actions={
+        isEditing ? (
+          <div className="ml-2 flex items-center gap-2">
+            <Button
+              aria-label="Confirm name"
+              onClick={handleConfirm}
+              className={updating ? "cursor-not-allowed opacity-50" : ""}
+              disabled={updating}
+            >
+              <div className="flex items-center gap-2">
+                <Save size={12} />
+                <span>Save</span>
+              </div>
+            </Button>
+            <Button
+              aria-label="Cancel edit"
+              variant="secondary"
+              onClick={handleCancel}
+              className={updating ? "cursor-not-allowed opacity-50" : ""}
+            >
+              <div className="flex items-center gap-2">
+                <XCircle size={12} />
+                <span>Cancel</span>
+              </div>
+            </Button>
+          </div>
+        ) : (
+          <Button variant="secondary" aria-label="Edit name" onClick={handleEditClick} className="ml-2">
+            <div className="flex items-center gap-2">
+              <Pencil size={12} />
+              <span>Edit</span>
+            </div>
+          </Button>
+        )
+      }
+    />
+  );
+};
 
 export default BoardNameField;
