7b0195d525be69b6bc87e7a44ff08f65e33fffa4
Adam Biśta <410788@student.agh.edu.pl>
2024-10-08 21:03:40 +0200

    feat(MetaData)[OK-66] Add metadata for user boards main layout and board details

--- client/src/app/user-boards/[boardId]/layout.tsx
+++ client/src/app/user-boards/[boardId]/layout.tsx
@@ -1,9 +1,19 @@
 import React, { FC } from "react";
 import BoardSidebar from "@/components/board-details/BoardSidebar";
+import { Metadata } from "next";
 
 interface LayoutProps {
   children: React.ReactNode;
   params: { boardId: string };
+}
+
+export async function generateMetadata({ params }: LayoutProps): Promise<Metadata> {
+  const { boardId } = params;
+  //const boardData = await getBoardData(boardId); todo: retrieve board name for title
+  return {
+    title: `User board - ${boardId} | Obelisk`,
+    description: "User board main information",
+  };
 }
 const BoardLayout: FC<LayoutProps> = ({ children, params: { boardId } }) => {
   return (
