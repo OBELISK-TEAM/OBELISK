92a0a403d3b3b94ad67ab7e42d13ad4f97fc5f7d
Adam Biśta <410788@student.agh.edu.pl>
2024-10-09 19:32:55 +0200

    fix(BoardDetails)[OK-209] Remove icons

--- client/src/components/board-details/board-information/BoardInfoInputItem.tsx
+++ client/src/components/board-details/board-information/BoardInfoInputItem.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
 interface BoardInfoInputItemProps {
-  icon: React.ReactNode;
+  icon?: React.ReactNode;
   label: string;
   value: string;
   inputProps?: React.InputHTMLAttributes<HTMLInputElement>;
