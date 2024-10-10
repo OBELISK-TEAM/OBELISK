219b36b80689d77e752474126cdeb662bc356d2d
Wiktor Wilkusz <wiktorwilkusz13@gmail.com>
2024-10-10 08:24:47 +0200

    refactor(stats)[OK-220] Unify names (#124)

--- server/src/modules/stats/objects/objects.stats.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { ObjectsStatsService } from './objects.stats.service';
-
-@Controller('stats/object')
-export class ObjectsStatsController {
-  constructor(private readonly objectsStatsService: ObjectsStatsService) {}
-}
--- server/src/modules/stats/slides/slides.stats.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SlidesStatsService } from './slides.stats.service';
-import { SlidesStatsController } from './slides.stats.controller';
-
-@Module({
-  controllers: [SlidesStatsController],
-  providers: [SlidesStatsService],
-})
-export class SlidesStatsModule {}
