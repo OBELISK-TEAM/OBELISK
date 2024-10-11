3436864b4a78dd174b5904b418b2220d6cfa33ba
Paweł Konop <101901301+marcepanowyy@users.noreply.github.com>
2024-10-11 19:48:55 +0200

    fix(stats)[OK-226] Add mongoose module to board stats (#127)

--- server/src/modules/stats/board/board.stats.module.ts
+++ server/src/modules/stats/board/board.stats.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { BoardStatsService } from './board.stats.service';
 import { BoardStatsController } from './board.stats.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  BoardStats,
+  BoardStatsSchema,
+} from '../../../schemas/stats/board.stats.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: BoardStats.name,
+        schema: BoardStatsSchema,
+      },
+    ]),
+  ],
   controllers: [BoardStatsController],
   providers: [BoardStatsService],
 })
