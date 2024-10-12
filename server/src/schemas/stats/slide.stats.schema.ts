ec5109c3415816b2a52a22b76e656efdf547c52b
Wajktor13 <wiktorwilkusz13@gmail.com>
2024-10-12 11:14:52 +0200

    feat(slide-stats)[OK-189] Add schema and basic logic

---
+++ server/src/schemas/stats/slide.stats.schema.ts
@@ -0,0 +1,70 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+import { StatsUserTimestamp } from 'src/shared/interfaces/StatsUserTimestamp';
+import { Schema as MongooseSchema } from 'mongoose';
+import { SuperSlide } from '../slide/super.slide.schema';
+import { User } from '../user.schema';
+
+@Schema()
+export class SlideStats extends Document {
+  @Prop({
+    type: Types.ObjectId,
+    ref: SuperSlide.name,
+    required: true,
+  })
+  slideId: Types.ObjectId;
+
+  @Prop({
+    type: Types.ObjectId,
+    ref: User.name,
+    required: true,
+  })
+  ownerId: Types.ObjectId;
+
+  @Prop({
+    type: MongooseSchema.Types.Mixed,
+    required: false,
+    default: [],
+  })
+  viewTimeline: StatsUserTimestamp;
+
+  @Prop({
+    type: MongooseSchema.Types.Mixed,
+    required: false,
+    default: [],
+  })
+  editTimeline: [
+    {
+      timestamp: Date;
+      userId: Types.ObjectId;
+      x: number;
+      y: number;
+      actionType: string;
+    },
+  ];
+
+  @Prop({
+    type: MongooseSchema.Types.Mixed,
+    required: false,
+    default: [],
+  })
+  objectsAddedTimeline: [{ timestamp: Date; objectsCount: number }];
+
+  @Prop({
+    type: MongooseSchema.Types.Mixed,
+    required: false,
+    default: [],
+  })
+  activeUsersTimeline: [{ timestamp: Date; usersCount: number }];
+
+  @Prop({
+    type: MongooseSchema.Types.Mixed,
+    required: false,
+    default: [],
+  })
+  timeSpent: [
+    { userId: Types.ObjectId; startDate: Date; endDate: Date | null },
+  ];
+}
+
+export const SlideStatsSchema = SchemaFactory.createForClass(SlideStats);
