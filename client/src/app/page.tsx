bd2f5e13a4c321f9707d6d948cfbac83377a880c
Adam Biśta <adamosekk2002@gmail.com>
2024-10-12 23:45:35 +0200

    feat(HomePage)[OK-231] Automatic redirection to /user-boards from the root route (#131)
    Co-authored-by: Adam Biśta <410788@student.agh.edu.pl>

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,13 +1,18 @@
+import { redirect } from "next/navigation";
+import logger from "@/lib/logger";
 const Home = () => {
-  return (
-    <div className="flex">
-      <div className="flex-grow p-10">
-        <h1>Online Board for Effective Learning, Interaction and Sharing Knowledge</h1>
-        <p>We Are The Future...</p>
-        <button className="rounded-lg bg-blue-500 p-3 text-white">Get Started!</button>
-      </div>
-    </div>
-  );
+  logger.log("Redirecting to /user-boards");
+  redirect("/user-boards");
+  // todo: JIRA[OK-39] - create a home page
+  // return (
+  //   <div className="flex">
+  //     <div className="flex-grow p-10">
+  //       <h1>Online Board for Effective Learning, Interaction and Sharing Knowledge</h1>
+  //       <p>We Are The Future...</p>
+  //       <button className="rounded-lg bg-blue-500 p-3 text-white">Get Started!</button>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default Home;
