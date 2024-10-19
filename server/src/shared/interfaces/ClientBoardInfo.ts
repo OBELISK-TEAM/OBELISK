eaab96697c05f6562b7fb6989209004a7c0acb63
Pawel <pawel.konop@gmail.com>
2024-10-19 22:54:39 +0200

    fix[OK-243] Fix imports

--- server/src/shared/interfaces/ClientBoardInfo.ts
+++ server/src/shared/interfaces/ClientBoardInfo.ts
@@ -1,6 +1,5 @@
-
 import { BoardWithSlideCount } from './BoardWithSlideCount';
-import { BoardPermission } from '../../enums/board.permission';
+import { BoardPermission } from '../enums/board.permission';
 
 export interface ClientBoardInfo
   extends Omit<BoardWithSlideCount, 'permissions'> {
