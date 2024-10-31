d8b971b7ffb81ec0b7003af46ec90831aa5ac597
Adam Biśta <410788@student.agh.edu.pl>
2024-10-31 13:35:26 +0100

    feat(comment)[OK-283] Add comments

--- client/src/app/user-boards/(realtime-canvas)/[boardId]/slides/[slideIndex]/page.tsx
+++ client/src/app/user-boards/(realtime-canvas)/[boardId]/slides/[slideIndex]/page.tsx
@@ -3,7 +3,7 @@ import BoardHorizontalMenu from "@/components/board/HorizontalMenu";
 import { MenuGroups } from "@/enums/MenuGroups";
 import SlideCanvas from "@/components/board/Canvas";
 import SlideFileInputs from "@/components/board/SlideFileInputs";
-import BoardToolBar from "@/components/board/toolbar/Toolbar";
+///import BoardToolBar from "@/components/board/toolbar/Toolbar";
 
 const Board: React.FC = () => {
   return (
@@ -18,7 +18,7 @@ const Board: React.FC = () => {
             width: `calc(100% - ${2 * 56}px)`,
           }}
         >
-          <BoardToolBar />
+          {/*<BoardToolBar />*/}
           <SlideCanvas />
           <SlideFileInputs />
         </div>
