e8fa3affb91ab119ab08c181b08151caf08b1b60
Adam Biśta <410788@student.agh.edu.pl>
2024-10-31 15:24:27 +0100

    fix(ColorUtils)[OK-283] Adjust parameters in constrasting color

--- client/src/lib/colorUtils.ts
+++ client/src/lib/colorUtils.ts
@@ -81,7 +81,7 @@ export const getContrastingTextColor = (backgroundColor: string): string => {
   const g = parseInt(hex.substring(2, 4), 16);
   const b = parseInt(hex.substring(4, 6), 16);
 
-  const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+  const luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
 
   return luminance > 0.5 ? "black" : "white";
 };
