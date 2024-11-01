84396dc5fc3f1595c8d777b6360bf1e90e5661fa
Pawel <pawel.konop@gmail.com>
2024-11-01 18:33:11 +0100

    feat(pipe)[OK-284] Add pipe config

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,55 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import {
-  DEFAULT_GW_PORT,
-  DEFAULT_RABBITMQ_HOST,
-  DEFAULT_RABBITMQ_PORT,
-  DEFAULT_SERVER_HOST,
-  DEFAULT_SERVER_PORT,
-} from './config/dev.config';
+import { DEFAULT_SERVER_PORT } from './config/dev.config';
 import { MicroserviceOptions, RmqOptions } from '@nestjs/microservices';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { getRabbitConfig } from './config/rabbit.config';
 import { getCorsConfig } from './config/cors.config';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { logServerInfo } from './config/logger.config';
+import { getGlobalPipeConfig } from './config/global.pipes.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
 
-  const serverHost = configService.get<string>(
-    'SERVER_HOST',
-    DEFAULT_SERVER_HOST,
-  );
   const serverPort = configService.get<number>(
     'SERVER_PORT',
     DEFAULT_SERVER_PORT,
   );
-  const rabbitmqHost = configService.get<string>(
-    'RABBITMQ_HOST',
-    DEFAULT_RABBITMQ_HOST,
-  );
-  const rabbitmqPort = configService.get<number>(
-    'RABBITMQ_PORT',
-    DEFAULT_RABBITMQ_PORT,
-  );
-  const gatewayPort = configService.get<number>(
-    'SOCKET_GW_PORT',
-    DEFAULT_GW_PORT,
-  );
 
-  // global validation pipe
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true, // removes properties that are not defined in the DTO
-      forbidNonWhitelisted: true, // throws an error if there are properties that are not defined in the DTO
-      transform: true, // automatically transforms input data to the expected types based on the DTO
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
-    }),
-  );
+  const pipe: ValidationPipe = getGlobalPipeConfig();
+  app.useGlobalPipes(pipe);
 
   const corsConfig: CorsOptions = getCorsConfig(configService);
   app.enableCors(corsConfig);
@@ -59,18 +30,7 @@ async function bootstrap() {
   await app.startAllMicroservices();
 
   await app.listen(serverPort);
-  Logger.log(
-    `Server running at http://${serverHost}:${serverPort}`,
-    'Bootstrap',
-  );
-  Logger.log(
-    `Socket gateway running at ws://${serverHost}:${gatewayPort}/gateway`,
-    'Bootstrap',
-  );
-  Logger.log(
-    `RabbitMQ microservice running at amqp://${rabbitmqHost}:${rabbitmqPort}`,
-    'Bootstrap',
-  );
+  logServerInfo(configService);
 }
 
 void bootstrap();
