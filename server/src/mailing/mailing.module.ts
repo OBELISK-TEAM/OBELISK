8e49de4daf3a1a93f950182ae2c99483ee43e06b
Pawel <pawel.konop@gmail.com>
2024-11-02 12:32:28 +0100

    test[OK-284] Test commit

--- server/src/mailing/mailing.module.ts
+++ server/src/mailing/mailing.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ProducerModule } from './producer/producer.module';
 import { ConsumerModule } from './consumer/consumer.module';
 
+// test
+
 @Module({
   imports: [ProducerModule, ConsumerModule],
 })
