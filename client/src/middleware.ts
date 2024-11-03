1d47449387e8a17463238184d86ce244ffc35e17
Adam Biśta <410788@student.agh.edu.pl>
2024-11-03 23:35:58 +0100

    fix(middleware)[OK-292] Remove import

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { decodeToken } from "@/lib/authApiUtils";
-import logger from "@/lib/logger";
 
 function isAuthenticated(request: NextRequest): boolean {
   const token = request.cookies.get("accessToken")?.value;
