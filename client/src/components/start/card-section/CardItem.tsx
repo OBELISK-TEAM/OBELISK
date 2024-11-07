545069a2ce03f6c78dd695f496bd1c25f17e90c4
Adam Biśta <410788@student.agh.edu.pl>
2024-11-07 22:56:41 +0100

    fix(start-page)[OK-39] Fix problems

--- client/src/components/start/card-section/CardItem.tsx
+++ client/src/components/start/card-section/CardItem.tsx
@@ -13,7 +13,7 @@ export const CardItem = ({ card }: CardSectionProps) => {
   const [imgSrc, setImgSrc] = useState(card.imageSrc || fallbackImage);
 
   return (
-    <div className="flex flex-1 cursor-pointer flex-col transition-all delay-150 duration-300 md:hover:scale-110">
+    <div className="flex flex-1 flex-col transition-all delay-150 duration-300 md:hover:scale-110">
       {card.imageSrc && (
         <div className="relative top-2 h-[10em] w-full">
           <Image
@@ -29,7 +29,7 @@ export const CardItem = ({ card }: CardSectionProps) => {
         </div>
       )}
       <Card className={`flex flex-1 flex-col gap-2 px-8 ${card.imageSrc ? "border-t-0 py-4" : "py-8"}`}>
-        <CardTitle className={imgSrc ? "text-md text-center" : "text-md"}>{card.title}</CardTitle>
+        <CardTitle className={imgSrc ? "text-md mt-2" : "text-md text-left"}>{card.title}</CardTitle>
         <CardDescription className="leading-6">{card.description}</CardDescription>
       </Card>
     </div>
