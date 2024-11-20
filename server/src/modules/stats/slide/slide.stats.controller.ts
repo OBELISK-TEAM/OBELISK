701447f3251e0eb5dd6258f306633682bb7aca28
Wiktor Wilkusz <wiktorwilkusz13@gmail.com>
2024-11-20 20:39:50 +0100

    feat(slide-stats)[OK-193] Add endpoint for fetching heatmap for slide actions (#202)

--- server/src/modules/stats/slide/slide.stats.controller.ts
+++ server/src/modules/stats/slide/slide.stats.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
 import { SlideStatsService } from './slides.stats.service';
+import { JwtAuthGuard } from 'src/modules/auth/guards/jwt.auth.guard';
 
 @Controller('stats/slide')
 export class SlideStatsController {
   constructor(private readonly slideStatsService: SlideStatsService) {}
+
+  @Get(':slideId/heatmap')
+  @UseGuards(JwtAuthGuard)
+  async getActionsHeatmapData(
+    @Param('slideId') slideId: string,
+    @Query('startDate') startDate: string,
+    @Query('endDate') endDate: string,
+  ) {
+    return this.slideStatsService.getActionsHeatmapData(
+      slideId,
+      new Date(startDate),
+      new Date(endDate),
+    );
+  }
 }
--- server/src/modules/stats/slide/slides.stats.service.ts
+++ server/src/modules/stats/slide/slides.stats.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { SlideStats } from 'src/mongo/schemas/stats/slide.stats.schema';
 import { SlideAction } from 'src/shared/enums/actions/slide.action';
+import { HeatmapPoint } from 'src/shared/interfaces/stats/HeatmapPoint';
 
 @Injectable()
 export class SlideStatsService {
@@ -102,5 +103,33 @@ export class SlideStatsService {
         },
       },
     );
+  }
+
+  async getActionsHeatmapData(
+    slideId: string,
+    startDate: Date,
+    endDate: Date,
+  ): Promise<HeatmapPoint[]> {
+    if (startDate.toString() === 'Invalid Date') startDate = new Date(0);
+    if (endDate.toString() === 'Invalid Date') endDate = new Date();
+
+    if (startDate >= endDate)
+      throw new HttpException('Inavlid dates', HttpStatus.BAD_REQUEST);
+
+    const slideStats = await this.slideStatsModel.findOne({
+      slideId,
+      'actionTimeline.timestamp': { $gte: startDate, $lte: endDate },
+    });
+
+    if (!slideStats || !slideStats.actionTimeline) return [];
+
+    const heatmapPoints: HeatmapPoint[] = slideStats.actionTimeline
+      .filter(action => action.top && action.left)
+      .map(action => ({
+        top: action.top || 0,
+        left: action.left || 0,
+      }));
+
+    return heatmapPoints;
   }
 }
